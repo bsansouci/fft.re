025122dd7d9cfd4938809eab2fd89bc1900b8aec
Benjamin San Souci <benjamin.sansouci@gmail.com>
2018-08-21 16:56:08 -0400

    Rename bundle

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import resolve from 'rollup-plugin-node-resolve';
 export default {
   input: 'lib/es6/src/fft.js',
   output: {
-    file: 'bundle.js',
+    file: 'index.js',
     format: 'iife'
   },
   name: 'MyModule',
